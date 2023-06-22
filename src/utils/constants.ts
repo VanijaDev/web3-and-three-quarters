fcff286148192f89d048fa3d37a6ec169979e7e7
Ivan Solomichev <vanijatko.social@gmail.com>
2023-06-22 17:17:53 -0500

    Updated constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,9 @@
 const minPassphraseLength = 15;
 const invalidPassphraseDuringWalletGeneration = `Invalid passphrase. Please ensure your passphrase is strong enough - minimum ${minPassphraseLength} characters, at least one uppercase letter, one lowercase letter, one number and one special character (*.!@#$%^&*()_=+<>-)`;
+const errorEmpty = {
+  encryptedWallet: "Empty string for encrypted wallet.",
+  passphrase: "Empty string for passphrase."
+};
+const failedToDecrypt = "Failed to decrypt wallet.";
 
-export { minPassphraseLength, invalidPassphraseDuringWalletGeneration };
+export { minPassphraseLength, invalidPassphraseDuringWalletGeneration, errorEmpty, failedToDecrypt };
