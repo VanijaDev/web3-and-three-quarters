796539f2acd174f045a14f528ef4065ae6340e60
Ivan Solomichev <vanijatko.social@gmail.com>
2023-07-06 16:16:32 -0500

    moved index file to root

---
+++ index.ts
@@ -0,0 +1,1 @@
+export { generateWallet, encryptWallet, decryptWallet, signMessage, getMessageSigner, isMessageSigner, signTransaction } from "./src/wallet_manager/wallet";
--- src/index.ts
+++
@@ -1,1 +0,0 @@
-export { generateWallet } from "./wallet_manager/wallet";
